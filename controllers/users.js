9f8f309419fe034c815e60e792f5e578956a3027
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-17 00:25:50 +0300

    initial commit

--- controllers/users.js
+++ controllers/users.js
@@ -28,16 +28,27 @@ usersRouter.get('/:id', async (request, response, next) => {
 
 usersRouter.post('/', async (request, response, next) => {
     const { username, name, email, password } = request.body
-    const passwordHash = await bcrypt.hash(password,10)
+   
     try {
+        const passwordHash = await bcrypt.hash(password,10)
+        
         const userObject = new User({ username, name, email, passwordHash })
         const savedUser = await userObject.save()
         response.status(201).json(savedUser)
-        response.sendStatus(201)
     } catch (error) {
         next(error)
     }
 })
+
+usersRouter.delete('/:id', async(request,response,next) => {
+    try {
+        await User.findByIdAndDelete(request.params.id)
+        response.sendStatus(204)
+    } catch (error) {
+        next(error)
+    }
+})
+
 
 // usersRouter.get('/', async(request,response,next) => {
 //     try {
