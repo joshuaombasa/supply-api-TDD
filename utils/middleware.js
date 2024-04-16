0c17585be195fa4f6c2e4db42f9f15d95d71f630
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-17 00:25:32 +0300

    initial commit

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,7 +17,9 @@ const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === 'CastError') {
-        return response.status(400).json({error: 'malformatted id'})
+        return response.status(400).json({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
