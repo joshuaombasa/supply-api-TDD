463c1b3a56afe4f3868de7b0d7bb08dc65df1318
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-16 23:53:26 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const usersRouter = require('./controllers/users')
+
+mongoose.connect(config.MONGO_URI)
+     .then(() => logger.info('connected to mongodb'))
+     .catch(() => logger.error(error.message))
+     
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpointHandler)
+app.use(middleware.errorHandler)
+
+module.exports = app
