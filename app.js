9b8acc23f330b1ab3cbbc953f49d50647fe6e2d8
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-17 21:37:43 +0300

    initial commit

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
 const usersRouter = require('./controllers/users')
+const goodsRouter = require('./controllers/goods')
 
 mongoose.connect(config.MONGO_URI)
      .then(() => logger.info('connected to mongodb'))
      .catch(() => logger.error(error.message))
-     
+
 const app = express()
 
 app.use(cors())
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
+app.use('/api/goods', goodsRouter)
 
 app.use(middleware.unknownEndpointHandler)
 app.use(middleware.errorHandler)
